96b39997bed0b720e74d66c3d731aff1230e3833
IagoRian <iagorian1212@gmail.com>
2020-11-21 19:51:53 -0300

    Exercício Loop

---
+++ 1000_1500/src/pkg1000_1500/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1000_1500;
+import java.util.Scanner;
+/**
+ *
+ * @author Naja
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int i, a;
+        i = 1000;
+        while(i<1500){
+        a = i+2;
+        System.out.println(a);
+        i = i+2;  
+        }
+// TODO code application logic here
+    }
+    
+}
